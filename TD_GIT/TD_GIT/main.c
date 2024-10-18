486deec20dcf6e93200cf2bcd99b530dd081f1a4
Wilhelm Macaigne <wmacaigne@gmail.com>
2024-10-18 09:24:28 +0200

    modif wilhelm 2

--- TD_GIT/TD_GIT/main.c
+++ TD_GIT/TD_GIT/main.c
@@ -4,6 +4,6 @@ int main()
 {
 	printf("Hello");
 	printf(" World !");
-	printf("coucou c’est moi ton nouveau binôme")
-	printf("coucou c'est ton duo")
+	printf("coucou c’est moi ton nouveau binôme");
+	printf("coucou c'est ton duo");
 }
